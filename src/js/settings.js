445065909dbe1718dbf8dde7e27c05e6a309137b
raffleberry <raffleberry42@gmail.com>
2019-03-09 03:53:27 +0530

    Added Language Modes

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,66 +1,264 @@
 /* DONT MIND ME: START */
 var themes = [
-  "ace/theme/gob",
-  "ace/theme/dawn",
-  "ace/theme/chaos",
-  "ace/theme/xcode",
-  "ace/theme/chrome",
-  "ace/theme/clouds",
-  "ace/theme/cobalt",
-  "ace/theme/github",
-  "ace/theme/kuroir",
-  "ace/theme/dracula",
-  "ace/theme/eclipse",
-  "ace/theme/gruvbox",
-  "ace/theme/monokai",
-  "ace/theme/ambiance",
-  "ace/theme/iplastic",
-  "ace/theme/kr_theme",
-  "ace/theme/terminal",
-  "ace/theme/textmate",
-  "ace/theme/tomorrow",
-  "ace/theme/twilight",
-  "ace/theme/merbivore",
-  "ace/theme/sqlserver",
-  "ace/theme/dreamweaver",
-  "ace/theme/katzenmilch",
-  "ace/theme/vibrant_ink",
-  "ace/theme/idle_fingers",
-  "ace/theme/crimson_editor",
-  "ace/theme/merbivore_soft",
-  "ace/theme/pastel_on_dark",
-  "ace/theme/solarized_dark",
-  "ace/theme/tomorrow_night",
-  "ace/theme/clouds_midnight",
-  "ace/theme/mono_industrial",
-  "ace/theme/solarized_light",
-  "ace/theme/tomorrow_night_blue",
-  "ace/theme/tomorrow_night_bright",
-  "ace/theme/tomorrow_night_eighties"
+  ["ace/theme/gob", "Gob"],
+  ["ace/theme/dawn", "Dawn"],
+  ["ace/theme/chaos", "Chaos"],
+  ["ace/theme/xcode", "Xcode"],
+  ["ace/theme/chrome", "Chrome"],
+  ["ace/theme/clouds", "Clouds"],
+  ["ace/theme/cobalt", "Cobalt"],
+  ["ace/theme/github", "Github"],
+  ["ace/theme/kuroir", "Kuroir"],
+  ["ace/theme/dracula", "Dracula"],
+  ["ace/theme/eclipse", "Eclipse"],
+  ["ace/theme/gruvbox", "Gruvbox"],
+  ["ace/theme/monokai", "Monokai"],
+  ["ace/theme/ambiance", "Ambiance"],
+  ["ace/theme/iplastic", "iPlastic"],
+  ["ace/theme/kr_theme", "KR_theme"],
+  ["ace/theme/terminal", "Terminal"],
+  ["ace/theme/textmate", "Textmate"],
+  ["ace/theme/tomorrow", "Tomorrow"],
+  ["ace/theme/twilight", "Twilight"],
+  ["ace/theme/merbivore", "Merbivore"],
+  ["ace/theme/sqlserver", "SQLserver"],
+  ["ace/theme/dreamweaver", "Dreamweaver"],
+  ["ace/theme/katzenmilch", "Karzenmilch"],
+  ["ace/theme/vibrant_ink", "Vibrant Ink"],
+  ["ace/theme/idle_fingers", "Idle Fingers"],
+  ["ace/theme/crimson_editor", "Crimson Editor"],
+  ["ace/theme/merbivore_soft", "Merbivore Soft"],
+  ["ace/theme/pastel_on_dark", "Pastel on Dark"],
+  ["ace/theme/solarized_dark", "Solarized Dark"],
+  ["ace/theme/tomorrow_night", "Tomorrow Night"],
+  ["ace/theme/clouds_midnight", "Clouds Midnight"],
+  ["ace/theme/mono_industrial", "Mono Industrial"],
+  ["ace/theme/solarized_light", "Solarized Light"],
+  ["ace/theme/tomorrow_night_blue", "Tomorrow Night Blue"],
+  ["ace/theme/tomorrow_night_bright", "Tommorow Night Bright"],
+  ["ace/theme/tomorrow_night_eighties", "Tomorrow Night Eighties"]
 ]
+
+var modes = [
+  ["ace/mode/abap", "ABAP"],
+  ["ace/mode/abc", "ABC"],
+  ["ace/mode/actionscript", "ActionScript"],
+  ["ace/mode/ada", "ADA"],
+  ["ace/mode/apache_conf", "Apache Conf"],
+  ["ace/mode/asciidoc", "AsciiDoc"],
+  ["ace/mode/asl", "ASL"],
+  ["ace/mode/assembly_x86", "Assembly x86"],
+  ["ace/mode/autohotkey", "AutoHotKey"],
+  ["ace/mode/apex", "Apex"],
+  ["ace/mode/batchfile", "BatchFile"],
+  ["ace/mode/bro", "Bro"],
+  ["ace/mode/c_cpp", "C Cpp"],
+  ["ace/mode/c9search", "C9Search"],
+  ["ace/mode/cirru", "Cirru"],
+  ["ace/mode/clojure", "Clojure"],
+  ["ace/mode/cobol", "Cobol"],
+  ["ace/mode/coffee", "coffee"],
+  ["ace/mode/coldfusion", "ColdFusion"],
+  ["ace/mode/csharp", "CSharp"],
+  ["ace/mode/csound_document", "Csound Document"],
+  ["ace/mode/csound_orchestra", "Csound Orchestra"],
+  ["ace/mode/csound_score", "Csound Score"],
+  ["ace/mode/css", "CSS"],
+  ["ace/mode/curly", "Curly"],
+  ["ace/mode/d", "D"],
+  ["ace/mode/dart", "Dart"],
+  ["ace/mode/diff", "Diff"],
+  ["ace/mode/dockerfile", "Dockerfile"],
+  ["ace/mode/dot", "Dot"],
+  ["ace/mode/drools", "Drools"],
+  ["ace/mode/edifact", "Edifact"],
+  ["ace/mode/eiffel", "Eiffel"],
+  ["ace/mode/ejs", "EJS"],
+  ["ace/mode/elixir", "Elixir"],
+  ["ace/mode/elm", "Elm"],
+  ["ace/mode/erlang", "Erlang"],
+  ["ace/mode/forth", "Forth"],
+  ["ace/mode/fortran", "Fortran"],
+  ["ace/mode/fsharp", "FSharp"],
+  ["ace/mode/fsl", "FSL"],
+  ["ace/mode/ftl", "FTL"],
+  ["ace/mode/gcode", "Gcode"],
+  ["ace/mode/gherkin", "Gherkin"],
+  ["ace/mode/gitignore", "Gitignore"],
+  ["ace/mode/glsl", "Glsl"],
+  ["ace/mode/gobstones", "Gobstones"],
+  ["ace/mode/golang", "golang"],
+  ["ace/mode/graphqlschema", "GraphQLSchema"],
+  ["ace/mode/groovy", "Groovy"],
+  ["ace/mode/haml", "HAML"],
+  ["ace/mode/handlebars", "Handlebars"],
+  ["ace/mode/haskell", "Haskell"],
+  ["ace/mode/haskell_cabal", "Haskell Cabal"],
+  ["ace/mode/haxe", "haXe"],
+  ["ace/mode/hjson", "Hjson"],
+  ["ace/mode/html", "HTML"],
+  ["ace/mode/html_elixir", "HTML Elixir"],
+  ["ace/mode/html_ruby", "HTML Ruby"],
+  ["ace/mode/ini", "INI"],
+  ["ace/mode/io", "Io"],
+  ["ace/mode/jack", "Jack"],
+  ["ace/mode/jade", "Jade"],
+  ["ace/mode/java", "Java"],
+  ["ace/mode/javascript", "JavaScript"],
+  ["ace/mode/json", "JSON"],
+  ["ace/mode/jsoniq", "JSONiq"],
+  ["ace/mode/jsp", "JSP"],
+  ["ace/mode/jssm", "JSSM"],
+  ["ace/mode/jsx", "JSX"],
+  ["ace/mode/julia", "Julia"],
+  ["ace/mode/kotlin", "Kotlin"],
+  ["ace/mode/latex", "LaTeX"],
+  ["ace/mode/less", "LESS"],
+  ["ace/mode/liquid", "Liquid"],
+  ["ace/mode/lisp", "Lisp"],
+  ["ace/mode/livescript", "LiveScript"],
+  ["ace/mode/logiql", "LogiQL"],
+  ["ace/mode/lsl", "LSL"],
+  ["ace/mode/lua", "Lua"],
+  ["ace/mode/luapage", "LuaPage"],
+  ["ace/mode/lucene", "Lucene"],
+  ["ace/mode/makefile", "Makefile"],
+  ["ace/mode/markdown", "Markdown"],
+  ["ace/mode/mask", "Mask"],
+  ["ace/mode/matlab", "MATLAB"],
+  ["ace/mode/maze", "Maze"],
+  ["ace/mode/mel", "MEL"],
+  ["ace/mode/mixal", "MIXAL"],
+  ["ace/mode/mushcode", "MUSHCode"],
+  ["ace/mode/mysql", "MySQL"],
+  ["ace/mode/nix", "Nix"],
+  ["ace/mode/nsis", "NSIS"],
+  ["ace/mode/objectivec", "ObjectiveC"],
+  ["ace/mode/ocaml", "OCaml"],
+  ["ace/mode/pascal", "Pascal"],
+  ["ace/mode/perl", "Perl"],
+  ["ace/mode/perl6", "Perl6"],
+  ["ace/mode/pgsql", "pgSQL"],
+  ["ace/mode/php_laravel_blade", "PHP Laravel_blade"],
+  ["ace/mode/php", "PHP"],
+  ["ace/mode/puppet", "Puppet"],
+  ["ace/mode/pig", "Pig"],
+  ["ace/mode/powershell", "Powershell"],
+  ["ace/mode/praat", "Praat"],
+  ["ace/mode/prolog", "Prolog"],
+  ["ace/mode/properties", "Properties"],
+  ["ace/mode/protobuf", "Protobuf"],
+  ["ace/mode/python", "Python"],
+  ["ace/mode/r", "R"],
+  ["ace/mode/razor", "Razor"],
+  ["ace/mode/rdoc", "RDoc"],
+  ["ace/mode/red", "Red"],
+  ["ace/mode/rhtml", "RHTML"],
+  ["ace/mode/rst", "RST"],
+  ["ace/mode/ruby", "Ruby"],
+  ["ace/mode/rust", "Rust"],
+  ["ace/mode/sass", "SASS"],
+  ["ace/mode/scad", "SCAD"],
+  ["ace/mode/scala", "Scala"],
+  ["ace/mode/scheme", "Scheme"],
+  ["ace/mode/scss", "SCSS"],
+  ["ace/mode/sh", "SH"],
+  ["ace/mode/sjs", "SJS"],
+  ["ace/mode/slim", "Slim"],
+  ["ace/mode/smarty", "Smarty"],
+  ["ace/mode/snippets", "snippets"],
+  ["ace/mode/soy_template", "Soy Template"],
+  ["ace/mode/space", "Space"],
+  ["ace/mode/sql", "SQL"],
+  ["ace/mode/sqlserver", "SQLServer"],
+  ["ace/mode/stylus", "Stylus"],
+  ["ace/mode/svg", "SVG"],
+  ["ace/mode/swift", "Swift"],
+  ["ace/mode/tcl", "Tcl"],
+  ["ace/mode/terraform", "Terraform"],
+  ["ace/mode/tex", "Tex"],
+  ["ace/mode/text", "Text"],
+  ["ace/mode/textile", "Textile"],
+  ["ace/mode/toml", "Toml"],
+  ["ace/mode/tsx", "TSX"],
+  ["ace/mode/twig", "Twig"],
+  ["ace/mode/typescript", "Typescript"],
+  ["ace/mode/vala", "Vala"],
+  ["ace/mode/vbscript", "VBScript"],
+  ["ace/mode/velocity", "Velocity"],
+  ["ace/mode/verilog", "Verilog"],
+  ["ace/mode/vhdl", "VHDL"],
+  ["ace/mode/visualforce", "Visualforce"],
+  ["ace/mode/wollok", "Wollok"],
+  ["ace/mode/xml", "XML"],
+  ["ace/mode/xquery", "XQuery"],
+  ["ace/mode/yaml", "YAML"],
+  ["ace/mode/django", "Django"]
+]
+
 /* DONT MIND ME: END */
 
 const electron = require('electron')
 const ipc = electron.ipcRenderer
+const settings = require('electron-settings')
 
-themeSelector = document.getElementById('themeSelector')
+var themeSelector = document.getElementById('themeSelector')
+var fontSelector = document.getElementById('fontSelector')
+var modeSelector = document.getElementById('modeSelector')
 
+/* variables */
+var options = null
+var option = null
 
-/* adding themes to the theme list : START */
+/* adding themes to the list : START */
 
-var options = []
-var option = document.createElement('option')
+options = []
+option = document.createElement('option')
 
 for (var i = 0; i < themes.length; i++) {
-  option.text = themes[i].split('/')[2]
-  option.value = themes[i]
+  option.text = themes[i][1]
+  option.value = themes[i][0]
   options.push(option.outerHTML)
 }
 
 themeSelector.insertAdjacentHTML('beforeEnd', options.join('\n'))
 
+options = null
+option = null
+
 /* adding themes to the theme list : END */
 
+/* adding modes to the list : START */
+
+options = []
+option = document.createElement('option')
+
+for (var i in modes) {
+  option.text = modes[i][1]
+  option.value = modes[i][0]
+  options.push(option.outerHTML)
+}
+
+modeSelector.insertAdjacentHTML('beforeEnd', options.join('\n'))
+
+options = null
+option = null
+
+/* adding modes to the theme list : END */
+
+fontSelector.value = settings.get('editor.fontSize')
+themeSelector.value = settings.get('editor.theme')
+modeSelector.value = settings.get('editor.mode')
+
+
 themeSelector.addEventListener('change', function() {
-  ipc.send('themeChange', document.getElementById('themeSelector').value)
+  settings.set('editor.theme', String(themeSelector.value))
+})
+
+fontSelector.addEventListener('change', function() {
+  settings.set('editor.fontSize', String(fontSelector.value))
+})
+
+modeSelector.addEventListener('change', function() {
+  settings.set('editor.mode', String(modeSelector.value))
 })
