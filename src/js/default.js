c5ee89ad3683d3b5e1c06bff221b05d253db9084
raffleberry <raffleberry42@gmail.com>
2019-03-09 16:59:42 +0530

    Changed fontSize type from Number to String.

--- src/js/default.js
+++ src/js/default.js
@@ -1,27 +1,29 @@
 var editor = ace.edit("editor");
-const settings = require('electron-settings')
+const settings = require('electron').remote.require('electron-settings');
 
 /*
-setx.set('editor', {
+settings.set('editor', {
     theme: "ace/theme/xcode",
     mode: "ace/mode/c_cpp",
     tabSize: 2,
     softTabs: true,
     highlightActiveLine: true,
-    fontSize: 16
+    fontSize: "16px"
   }
 )
 */
 
+/*
+use these details to set electron-settings on first run
+or
+lots of shit WILL GO WRONG
+*/
+
+//editor-setup on startup
 editor.setTheme(settings.get('editor.theme'));
 editor.session.setMode(settings.get('editor.mode'));
 editor.setAutoScrollEditorIntoView(true);
 editor.session.setTabSize(settings.get('editor.tabSize'));
 editor.session.setUseSoftTabs(settings.get('editor.softTabs'));
 editor.setHighlightActiveLine(settings.get('editor.highlightActiveLine'));
-document.getElementById('editor').style.fontSize = String(settings.get('editor.fontSize'))+'px';
-/*
-use these details to set electron-settings on first run
-or
-lots of shit WILL GO WRONG
-*/
+document.getElementById('editor').style.fontSize = settings.get('editor.fontSize');
